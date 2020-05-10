e2f2b8e6d03f90eb5661ea7b1c0c0da2986c4793
willmorim <pdbawilliam@gmail.com>
2020-05-10 16:28:06 -0300

    Iput & Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
-
 import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça o seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('oi');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
