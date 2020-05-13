6f528d6748160a8b26bd6d18867df0a93f03a6dd
willmorim <pdbawilliam@gmail.com>
2020-05-12 21:35:47 -0300

    Foco e preenchimento

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,7 @@
 import React, {
   useEffect,
+  useState,
+  useCallback,
   useRef,
   useImperativeHandle,
   forwardRef,
@@ -27,10 +29,22 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   ref,
 ) => {
   const inputElementRef = useRef<any>(null);
+  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
 
   const { fieldName, registerField, defaultValue = '', error } = useField(name);
 
-  const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
 
   useImperativeHandle(ref, () => ({
     focus() {
@@ -47,14 +61,20 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
 
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={(value) => {
           inputValueRef.current.value = value;
         }}
